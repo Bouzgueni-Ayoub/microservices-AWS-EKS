e9c43466453742626afad65652bca8486201314a
ayoub bouzgueni <bouzgueniayoub@gmail.com>
2025-08-09 12:26:52 +0000

    junk

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,3 @@
 output "eks_cluster_sg" {
   value = aws_eks_cluster.eks_cluster.vpc_config[0].cluster_security_group_id
 }
-output "node_role_arn"{
-  value=aws_eks_node_group.eks_node_group.node_role_arn
-}
