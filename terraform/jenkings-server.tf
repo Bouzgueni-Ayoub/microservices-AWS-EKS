2d69c501d3b02d7cf0fd8ab6d5685db954730212
ayoub bouzgueni <bouzgueniayoub@gmail.com>
2025-08-10 11:24:12 +0000

    changing jenkins server to accommodate for imaging building power demand

--- terraform/jenkings-server.tf
+++ terraform/jenkings-server.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "jenkings" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
   subnet_id     = aws_subnet.public_subnet_1a.id
   associate_public_ip_address = true
   user_data                   = file("../user_data/user_data.sh")
